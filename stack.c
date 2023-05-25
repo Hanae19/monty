35d94c10616a6772ad8874bcef38d43ea358815c
Hanae19 <hasnaeyassine1977@gmail.com>
2023-05-25 16:05:06 -0700

    update

---
+++ stack.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * f_stack -prints tje top
+ * @head: stack head
+ * @counter: line_number
+ * Retur: no return
+ */
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi =0;
+}
